3bc87218a0bb28d276daec3cc34078be4c96b90a
phamxuanthuy248-glitch <phamxuanthuy248@gmail.com>
2026-04-21 06:11:39 +0700

    Create sidebar.php for admin sidebar navigation

---
+++ views/layout/sidebar.php
@@ -0,0 +1,9 @@
+<aside>
+    <nav>
+        <ul>
+            <li><a href="/admin/dashboard">Dashboard</a></li>
+            <li><a href="/admin/users">Users</a></li>
+            <li><a href="/admin/settings">Settings</a></li>
+        </ul>
+    </nav>
+</aside>
